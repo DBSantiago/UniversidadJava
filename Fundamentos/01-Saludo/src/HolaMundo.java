155272689d0d2f0fd02da0adefae841270646a79
Santiago De Blasis <71677491+DBSantiago@users.noreply.github.com>
2021-11-16 20:19:06 -0300

    Add files via upload

---
+++ Fundamentos/01-Saludo/src/HolaMundo.java
@@ -0,0 +1,9 @@
+public class HolaMundo {
+    public static void main(String args[]){
+        System.out.println("Hola Mundo!");
+
+        int miVariableEntera = 10;
+
+        System.out.println(miVariableEntera);
+    }
+}
